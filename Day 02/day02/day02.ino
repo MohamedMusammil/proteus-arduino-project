fb35d8690f06c40f7573d4eaf091da4b8742e68c
Musammil <141551674+MohamedMusammil@users.noreply.github.com>
2024-11-07 09:56:06 +0530

    Add files via upload

---
+++ Day 02/day02/day02.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+// Pin connection (RS, E, D4, D5, D6, D7)
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8); 
+
+void setup() {
+  lcd.begin(16, 2); //selected the arduino board 16x2
+  lcd.print("Musammil!"); 
+  lcd.begin
+  
+}
+
+void loop() {
+  
+}
